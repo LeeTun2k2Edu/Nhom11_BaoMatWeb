8b643716c09e485ff4492eb3b2e71adf3f53cca7
MinhQuan21112002 <93760661+MinhQuan21112002@users.noreply.github.com>
2023-05-30 17:53:50 +0700

    Update SignUpControl.java

--- src/main/java/Control/SignUpControl.java
+++ src/main/java/Control/SignUpControl.java
@@ -4,6 +4,7 @@ import DAO.LoaispDAO;
 import DAO.SanPhamDAO;
 import DAO.SignUpDAO;
 import Model.LoaiSP;
+import Model.MD5;
 import Model.SanPham;
 import Model.SendMail;
 import Model.Users;
@@ -16,7 +17,6 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import java.util.List;
-import java.util.regex.Pattern;
 
 /**
  * Servlet implementation class SignUpControl
@@ -24,8 +24,6 @@ import java.util.regex.Pattern;
 @WebServlet("/signup")
 public class SignUpControl extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-//	String invalidCharsRegex = ".*[\'\";%].*";
-	String invalidCharsRegex = ".*[\'\";%\\\\].*";
 
 	/**
 	 * @see HttpServlet#HttpServlet()
@@ -66,6 +64,10 @@ public class SignUpControl extends HttpServlet {
 		String password = request.getParameter("password");
 		String repassword = request.getParameter("repassword");
 
+		  MD5 lib = new MD5();
+	        String passMD5 = lib.md5(password);
+	        String repassMD5 = lib.md5(repassword);
+	        
 		SignUpDAO dao = new SignUpDAO();
 		Users a = dao.CheckUserExist(username);
 		if (a == null) {
@@ -77,23 +79,14 @@ public class SignUpControl extends HttpServlet {
 			session.setAttribute("username", username);
 			session.setAttribute("email", email);
 			session.setAttribute("phone", phone);
-			session.setAttribute("password", password);
-			session.setAttribute("repassword", repassword);
+			session.setAttribute("password", passMD5);
+			session.setAttribute("repassword", repassMD5);
 			session.setAttribute("verify", veri);
 
 			SendMail sm = new SendMail();
 			Boolean test = sm.sendMail(email, veri, fullname);
-			
-			if (Pattern.matches(invalidCharsRegex, username) || username.contains(" ")) {
-			    request.setAttribute("mess1", "Username chứa các kí tự không hợp lệ hoặc khoảng trắng .*[\\'\\\";%].*");
-			    request.getRequestDispatcher("/shop-cart/signUp.jsp").forward(request, response);
-			}
-			else if(!password.contentEquals(repassword)) {
-				request.setAttribute("mess1", "Mật khẩu không trùng khớp");
-				request.getRequestDispatcher("/shop-cart/signUp.jsp").forward(request, response);
-			}
-			
-			else if (test == false) {
+
+			if (test == false) {
 				request.setAttribute("mess1", "Email không chính xác");
 				request.getRequestDispatcher("/shop-cart/signUp.jsp").forward(request, response);
 			} else {
