5f27be9cd2e81b8d2e2674332ff72bfb32463e25
MinhQuan21112002 <93760661+MinhQuan21112002@users.noreply.github.com>
2023-06-04 19:22:21 +0700

    Update SignUpControl.java

--- src/main/java/Control/SignUpControl.java
+++ src/main/java/Control/SignUpControl.java
@@ -3,6 +3,7 @@ package Control;
 import DAO.LoaispDAO;
 import DAO.SanPhamDAO;
 import DAO.SignUpDAO;
+import Model.Argon2;
 import Model.LoaiSP;
 import Model.MD5;
 import Model.SanPham;
@@ -17,108 +18,94 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import java.util.List;
-import java.util.regex.Pattern;
 
 /**
  * Servlet implementation class SignUpControl
  */
 @WebServlet("/signup")
 public class SignUpControl extends HttpServlet {
-    private static final long serialVersionUID = 1L;
+	private static final long serialVersionUID = 1L;
 
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public SignUpControl() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public SignUpControl() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
 
-    /**
-     * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
-     *      response)
-     */
-    protected void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        List<LoaiSP> listLSp = new LoaispDAO().getAllloaisp();
-        request.setAttribute("listlSp", listLSp);
-        List<SanPham> listChuaRaMat = new SanPhamDAO().chuaRaMat();
-        request.setAttribute("chuaramat", listChuaRaMat);
-        request.getRequestDispatcher("/shop-cart/signUp.jsp").forward(request, response);
-    }
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		List<LoaiSP> listLSp = new LoaispDAO().getAllloaisp();
+		request.setAttribute("listlSp", listLSp);
+		List<SanPham> listChuaRaMat = new SanPhamDAO().chuaRaMat();
+		request.setAttribute("chuaramat", listChuaRaMat);
+		request.getRequestDispatcher("/shop-cart/signUp.jsp").forward(request, response);
+	}
 
-    /**
-     * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
-     *      response)
-     */
-    protected void doPost(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        request.setCharacterEncoding("UTF-8");
-        response.setCharacterEncoding("UTF-8");
-        response.setContentType("text/html; charset=UTF-8");
-        List<LoaiSP> listLSp = new LoaispDAO().getAllloaisp();
-        request.setAttribute("listlSp", listLSp);
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		request.setCharacterEncoding("UTF-8");
+		response.setCharacterEncoding("UTF-8");
+		response.setContentType("text/html; charset=UTF-8");
+		List<LoaiSP> listLSp = new LoaispDAO().getAllloaisp();
+		request.setAttribute("listlSp", listLSp);
+		String fullname = request.getParameter("fullname");
+		String username = request.getParameter("username");
+		String email = request.getParameter("email");
+		String phone = request.getParameter("phone");
+		String password = request.getParameter("password");
+		String repassword = request.getParameter("repassword");
 
-        String fullname = request.getParameter("fullname");
-        String username = request.getParameter("username");
-        String email = request.getParameter("email");
-        String phone = request.getParameter("phone");
-        String password = request.getParameter("password");
-        String repassword = request.getParameter("repassword");
+		Argon2 argon2 = new Argon2();
+		String passArgon2 = argon2.argon2(password);
+		String repasssArgon2 = argon2.argon2(repassword);
 
-        
-        // escape XSS 
-        fullname = escapeXML(fullname);
-        
-        
-        
-        MD5 lib = new MD5();
-        String passMD5 = lib.md5(password);
-        String repassMD5 = lib.md5(repassword);
+		System.out.println(passArgon2);
+		System.out.println(repasssArgon2);
+		
+		MD5 lib = new MD5(); 
+		String passMD5 = lib.md5(password); 
+		String repassMD5 =  lib.md5(repassword);
+		System.out.println(passMD5);
+		System.out.println(repassMD5);
 
-        SignUpDAO dao = new SignUpDAO();
-        Users a = dao.CheckUserExist(username);
-        
-        if (a == null) {
-            int veri = dao.getRandom();
+		SignUpDAO dao = new SignUpDAO();
+		Users a = dao.CheckUserExist(username);
+		if (a == null) {
 
-            HttpSession session = request.getSession();
-            session.setAttribute("fullname", fullname);
-            session.setAttribute("username", username);
-            session.setAttribute("email", email);
-            session.setAttribute("phone", phone);
-            session.setAttribute("password", passMD5);
-            session.setAttribute("repassword", repassMD5);
-            session.setAttribute("verify", veri);
+			int veri = dao.getRandom();
 
-            SendMail sm = new SendMail();
+			HttpSession session = request.getSession();
+			session.setAttribute("fullname", fullname);
+			session.setAttribute("username", username);
+			session.setAttribute("email", email);
+			session.setAttribute("phone", phone);
+			session.setAttribute("password", passArgon2);
+			session.setAttribute("repassword", passArgon2);
+			session.setAttribute("verify", veri);
 
-            Boolean test = sm.sendMail(email, veri, fullname);
+			SendMail sm = new SendMail();
+			Boolean test = sm.sendMail(email, veri, fullname);
 
-            if (username.matches(".*[\\'\\\";%].*") || username.contains(" ")) {
-                request.setAttribute("mess1", "Username chứa các kí tự không hợp lệ hoặc khoảng trắng .*[\\'\\\";%].*");
-                request.getRequestDispatcher("/shop-cart/signUp.jsp").forward(request, response);
-            } else if (!password.equals(repassword)) {
-                request.setAttribute("mess1", "Mật khẩu không trùng khớp");
-                request.getRequestDispatcher("/shop-cart/signUp.jsp").forward(request, response);
-            } else if (test == false) {
-                request.setAttribute("mess1", "Email không chính xác");
-                request.getRequestDispatcher("/shop-cart/signUp.jsp").forward(request, response);
-            } else {
-                response.sendRedirect("verify");
-            }
-        } else {
-            request.setAttribute("mess1", "Tài khoản đã tồn tại");
-            request.getRequestDispatcher("/shop-cart/signUp.jsp").forward(request, response);
-        }
-    }
-    
-    // Escape XML
-    private String escapeXML(String input) {
-        return input.replace("&", "&amp;")
-                .replace("<", "&lt;")
-                .replace(">", "&gt;")
-                .replace("\"", "&quot;")
-                .replace("'", "&apos;");
-    }
+			if (test == false) {
+				request.setAttribute("mess1", "Email không chính xác");
+				request.getRequestDispatcher("/shop-cart/signUp.jsp").forward(request, response);
+			} else {
+				response.sendRedirect("verify");
+			}
+		} else {
+			request.setAttribute("mess1", "Tài khoản đã tồn tại");
+			request.getRequestDispatcher("/shop-cart/signUp.jsp").forward(request, response);
+		}
+	}
+
 }
